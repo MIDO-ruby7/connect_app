910fc03dc75ec00407dddbcf391c5358e7215329
Midori Takahashi <mintgreen7programming@gmail.com>
2022-12-21 14:16:59 +0900

    rails g c  Usersessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
   root to: 'home#top'
   resources :users, only: %i[new create]
   resources :boards

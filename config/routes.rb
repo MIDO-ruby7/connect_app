c7773288b718ab956c25bb02381061a42fce55d2
Midori Takahashi <mintgreen7programming@gmail.com>
2022-12-21 13:42:55 +0900

    add sorcery first

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#top'
 
   resources :boards
-  
   #get 'home/index'
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_033714) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_043230) do
   create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
